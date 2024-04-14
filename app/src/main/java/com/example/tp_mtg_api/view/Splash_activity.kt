d1cbb1931f45a8f2e86c0f4bdbde3d3ba3d85c8d
Poesero <81367033+Poesero@users.noreply.github.com>
2024-04-14 19:00:35 -0300

    Added Splash_activity with delay before MainActivity

---
+++ app/src/main/java/com/example/tp_mtg_api/view/Splash_activity.kt
@@ -0,0 +1,26 @@
+package com.example.tp_mtg_api.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tp_mtg_api.R
+import com.example.tp_mtg_api.viewModel.MainActivity
+
+class Splash_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+}
