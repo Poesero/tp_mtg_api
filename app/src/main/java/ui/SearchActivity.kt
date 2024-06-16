289d320a38b9ef674e5f6bc0288d8207b1b67a7f
Poesero <81367033+Poesero@users.noreply.github.com>
2024-06-16 16:18:09 -0300

    SearchActivity & SearchViewModel (can't go from main to search for some reason, gotta look what's up)

--- app/src/main/java/ui/SearchActivity.kt
+++ app/src/main/java/ui/SearchActivity.kt
@@ -1,7 +1,25 @@
 package ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.tp_mtg_api.R
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var viewModel: SearchViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        bindViewModel()
+    }
+
+    override fun onStart(){
+        super.onStart()
+        viewModel.onStart()
+    }
+
+    private fun bindViewModel(){
+        viewModel = ViewModelProvider(this)[SearchViewModel::class.java]
+    }
 }
---
+++ app/src/main/java/ui/SearchViewModel.kt
@@ -0,0 +1,9 @@
+package ui
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    fun onStart(){
+
+    }
+}
