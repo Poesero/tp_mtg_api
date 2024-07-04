9fd44fc98e0e47acc0751974113673ec75e93d62
Poesero <81367033+Poesero@users.noreply.github.com>
2024-07-04 20:54:23 -0300

    final changes

--- app/src/main/java/data/CardsDataSource.kt
+++ app/src/main/java/data/CardsDataSource.kt
@@ -11,67 +11,71 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class CardsDataSource {
-    val db = FirebaseFirestore.getInstance()
-    private val _BASE_URL = "https://api.scryfall.com/"
-    private val _TAG = "API-CHECK"
+    companion object{
+        val db = FirebaseFirestore.getInstance()
+        private val api: CardsAPI
+        private val _BASE_URL = "https://api.scryfall.com/"
+        private val _TAG = "API-CHECK"
 
-    suspend fun getCardsColors(name: String,color: String): ArrayList<Card>{
-        Log.d(_TAG,"Cards Datasource GetColorCards")
-        delay(5000)
-        val api =Retrofit.Builder()
-            .baseUrl(_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build().create(CardsAPI::class.java)
-        return try {
-            val query ="$name color>=$color"
-            Log.d(_TAG,query)
-            val result = api.getCardsColors(query)
-            Log.d(_TAG, "Resultado Exitoso")
-            Log.d(_TAG, "${result.data} \n size:${result.data.size}")
-            result.data
-        } catch (e: Exception) {
-            Log.e(_TAG, "Error en llamado API: ${e.message}")
-            ArrayList<Card>()
+        init{
+            api = Retrofit.Builder()
+                .baseUrl(_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(CardsAPI::class.java)
+        }
+        suspend fun getCardsColors(name: String,color: String): ArrayList<Card>{
+            Log.d(_TAG,"Cards Datasource GetColorCards")
+            delay(5000)
+
+            return try {
+                val query ="$name color>=$color"
+                Log.d(_TAG,query)
+                val result = api.getCardsColors(query)
+                Log.d(_TAG, "Resultado Exitoso")
+                Log.d(_TAG, "${result.data} \n size:${result.data.size}")
+                result.data
+            } catch (e: Exception) {
+                Log.e(_TAG, "Error en llamado API: ${e.message}")
+                ArrayList<Card>()
+            }
+        }
+
+        suspend fun getRandom(): Card{
+            val api =Retrofit.Builder()
+                .baseUrl(_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(CardsAPI::class.java)
+            return try {
+                val result = api.getRandom()
+                result
+            } catch (e: Exception) {
+                Log.e(_TAG, "Error en llamado API: ${e.message}")
+                throw e
+            }
+        }
+
+        suspend fun getCards(name: String, context: Context): ArrayList<Card>{
+            Log.d(_TAG,"Cards Datasource Get")
+
+            var  db = AppDatabase.getInstance(context)
+            var cardLocal = db.cardsDao().getAll()
+            if (cardLocal.size>0){
+                return cardLocal.toCardList() as ArrayList<Card>
+            }
+
+            delay(5000)
+
+            var result = api.getCards(name)
+            return try {
+                val result = api.getCards(name)
+                Log.d(_TAG, "Resultado Exitoso")
+                Log.d(_TAG, "${result.data} \n size:${result.data.size}")
+                result.data
+            } catch (e: Exception) {
+                Log.e(_TAG, "Error en llamado API: ${e.message}")
+                ArrayList<Card>()
+            }
         }
     }
 
-    suspend fun getRandom(): Card{
-        val api =Retrofit.Builder()
-            .baseUrl(_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build().create(CardsAPI::class.java)
-        return try {
-            val result = api.getRandom()
-            result
-        } catch (e: Exception) {
-            Log.e(_TAG, "Error en llamado API: ${e.message}")
-            throw e
-        }
-    }
-
-    suspend fun getCards(name: String, context: Context): ArrayList<Card>{
-        Log.d(_TAG,"Cards Datasource Get")
-
-        var  db = AppDatabase.getInstance(context)
-        var cardLocal = db.cardsDao().getAll()
-        if (cardLocal.size>0){
-            return cardLocal.toCardList() as ArrayList<Card>
-        }
-
-        delay(5000)
-
-        val api =Retrofit.Builder()
-            .baseUrl(_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build().create(CardsAPI::class.java)
-        return try {
-            val result = api.getCards(name)
-            Log.d(_TAG, "Resultado Exitoso")
-            Log.d(_TAG, "${result.data} \n size:${result.data.size}")
-            result.data
-        } catch (e: Exception) {
-            Log.e(_TAG, "Error en llamado API: ${e.message}")
-            ArrayList<Card>()
-        }
-    }
 }
--- app/src/main/java/data/CardsRepo.kt
+++ app/src/main/java/data/CardsRepo.kt
@@ -4,17 +4,17 @@ import model.Card
 
 class CardsRepo {
 
-    private val cardsDS = CardsDataSource()
+
 
     suspend fun  getCards(name:String, context: Context) : ArrayList<Card>{
-        return cardsDS.getCards(name,context)
+        return CardsDataSource.getCards(name,context)
     }
 
     suspend fun getRandom(): Card {
-        return cardsDS.getRandom()
+        return CardsDataSource.getRandom()
     }
 
     suspend fun getCardsColors(name: String,color:String) : ArrayList<Card>{
-        return cardsDS.getCardsColors(name,color)
+        return CardsDataSource.getCardsColors(name,color)
     }
 }
